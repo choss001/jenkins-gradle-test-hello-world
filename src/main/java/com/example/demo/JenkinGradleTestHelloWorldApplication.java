a0949c6de4641c9d0852e8b7f9c67aca787f84d1
JSS <choss01@oneplat.co>
2020-08-04 11:04:20 +0900

    test

--- src/main/java/com/example/demo/JenkinGradleTestHelloWorldApplication.java
+++ src/main/java/com/example/demo/JenkinGradleTestHelloWorldApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinGradleTestHelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinGradleTestHelloWorldApplication.class, args);
-	}
+  @Value("${val}")
+  private String val;
+
+  @Autowired
+  private OrderService orderService;
+
+  // public JenkinGradleTestHelloWorldApplication (OrderService orderService) {
+  // this.orderService = orderService;
+  // }
+
+  public static void main(String[] args) {
+    SpringApplication.run(JenkinGradleTestHelloWorldApplication.class, args);
+  }
+
+   @PostConstruct
+   public void test() {
+   orderService.order();
+  
+   System.out.println("test : "+val);
+   }
 
 }
--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class controller {
   
+  
   @Value("${val}")
   private String val;
   
-  private OrderService orderService;
+  private final OrderService orderService;
 
   public controller (OrderService orderService) {
     this.orderService = orderService;
